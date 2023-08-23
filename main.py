1783161e63b1e5c368dbfffc6eecc87720db90a2
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-23 21:07:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 
 while True:
     # Open the bot.py file using the subprocess module
-    bot_process = subprocess.Popen(['python', 'bot.py'])
+    bot_process = subprocess.Popen(['python3', 'bot.py'])
     # Wait for given amount of seconds
     time.sleep(43200) # That's 12 hours
     

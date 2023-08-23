cbbcbf6d5f7a23db7fc1359f14827cebe24da7b8
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-23 20:48:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
+# Code made by https://github.com/EduContin
+# This script restarts bot.py every X amount of time to verify if memberships expired, and if so, user gets bannd/kicked.
+# More often restarts = more often bot verifies expired memberships
+# Recommended amount = 4-12 hours
+
 import subprocess
 import time
-import sys
 
 while True:
-    try:
-        # Open the bot.py file using the subprocess module
-        bot_process = subprocess.Popen([sys.executable, 'bot.py'])
-        
-        # Wait for the given amount of seconds
-        time.sleep(43200)  # 12 hours
-        
-        # Terminate the bot.py process
-        bot_process.terminate()
-        
-        # Wait a short while to ensure the process is terminated properly
-        time.sleep(5)
-        
-    except Exception as e:
-        print("An error occurred:", e)
-        # You might want to log or handle the error in a more appropriate way
+    # Open the bot.py file using the subprocess module
+    bot_process = subprocess.Popen(['python', 'bot.py'])
     
-    # Repeat the loop
+    # Wait for given amount of seconds
+    time.sleep(43200) # That's 12 hours
+    
+    # Terminate the bot.py process
+    bot_process.terminate()
